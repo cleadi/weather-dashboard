e3f1dd3f6c3b67eeec608d8893f112c42f2a8554
Dillon Cleaver <cleadi@fastmail.com>
2021-10-04 07:52:39 -0500

    Updated script.js and index.html files.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,26 @@
 // create variables for each item on HTML page
 
+var mainCityBox = $("#main-city-box");
+var mainCityDate = $("#main-city-date");
+var mainCityImg = $("#main-city-img");
+var mainCityTemp = $("#main-city-temp");
+var mainCityHum = $("#main-city-hum");
+var mainCityUv = $("#main-city-uv");
+
+var fiveDayForcastBox = $("#five-day-forecast");
+var fiveDayDate = $("#five-day-date");
+var fiveDayImg = $("#five-day-img");
+var fiveDayTemp = $("#five-day-temp");
+var fiveDayHum = $("#five-day-hum");
+var fiveDayUv = $("#five-day-uv");
+
 var apiKey = "bccdc8b00b9907f0babdf04baa3111c4";
-var queryUrl = "";
-var queryUrl2 = "";
+var queryUrl;
+var queryUrl2;
 var cityName = "";
-var lat;
-var lon;
+var lat = "";
+var lon = "";
+// var data;
 
 var searchButton = $("#search-button");
 var inputText = $("#input-text")[0];
@@ -14,32 +29,42 @@ searchButton.click(function(event) {
   event.preventDefault();
   cityName = inputText.value;
   getCityData(cityName);
-  console.log(cityName); // works, displays city name
 });
 
+// function weatherData() {
+
+// }
+
 function getCityData(cityName) {
-  queryUrl = "api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
-  console.log(queryUrl);
+  queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
+  // console.log(queryUrl);
   fetch(queryUrl)
     .then(function (response) {
-      return response.json();
-      })
-    .then(function(data) {
-      //... missing something here
-    })      
+      return Response.json();
+    })
+    .then(function (data) {
+      long = data.coords.longitude;
+      lat = data.coords.latitude;
+     })
 };
+
+// getCityData();
+// console.log(getCityData);
 
 function getLatLong(getCityData) {
   queryUrl2 = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
-  console.log(queryUrl2);
   fetch(queryUrl2)
     .then(function (response) {
-      return response.json();
+      return Response.json();
     })
     .then(function (data) {
-      //... missing something here
+      lon = data.coords.longitude;
+      lat = data.coords.latitude;
     })
 };
+
+getLatLong();
+console.log(getLatLong);
 
 
 // Gary says (on 09/29/21) the below will be needed for fetching data from an API
