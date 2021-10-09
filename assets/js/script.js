012b6b0cee549da3afaad938f792906df0e6ea1d
Dillon Cleaver <cleadi@fastmail.com>
2021-10-09 16:44:50 -0500

    Made a little progress with API. Still not populating on page though.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,13 @@
-// create variables for each item on HTML page
+// Need to pull in data to getCityData and getLatLong functions
+// Need to populate ids listed below with the related data and show on web page
+// Need to save recent searches to local storage and show underneath the search bar on left side of web page
+
 
 var mainCityBox = $("#main-city-box");
 var mainCityDate = $("#main-city-date");
 var mainCityImg = $("#main-city-img");
 var mainCityTemp = $("#main-city-temp");
+var mainCityWind = $("#main-city-wind");
 var mainCityHum = $("#main-city-hum");
 var mainCityUv = $("#main-city-uv");
 
@@ -15,64 +19,53 @@ var fiveDayHum = $("#five-day-hum");
 var fiveDayUv = $("#five-day-uv");
 
 var apiKey = "bccdc8b00b9907f0babdf04baa3111c4";
-var queryUrl;
-var queryUrl2;
-var cityName = $(".form-control"); // this is the issue with the getCityData API call, when I sub this variable with Minneapolis, there's no (API URL) 400 Bad Request error
-// var cityName = "";
+var cityName = "";
 var lat = "";
 var lon = "";
-// var data;
 
 var searchButton = $("#search-button");
-var inputText = $(".form-control");
-
-searchButton.click(function(event) {
-  event.preventDefault();
-  cityName = inputText.value;
-  getCityData(cityName);
-});
-
-// function weatherData() {
-
-// }
-
+var inputText = $("#input-city");
 
 function getCityData(cityName) {
-  console.log(cityName)
-  queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
-  // console.log(queryUrl);
-  fetch(queryUrl) // error, GET (API URL) 400 Bad Request
+  var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
+  fetch(queryUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data)
-      lon = data.coord.lon; // Uncaught (in promise) TypeError: Cannot read properties of undefined
+      lon = data.coord.lon;
       lat = data.coord.lat;
+      console.log(data);
       getLatLong();
-     })
-     // .catch?? do I need a var which is just for displaying something if the callback fails?
+    })
 };
 
-// .catch example from MDN -- https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Using_promises
-
-getCityData("Minneapolis");
-// console.log(JSON.stringify(getCityData)); // logs undefined
-
 function getLatLong(getCityData) {
-  queryUrl2 = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
-  console.log(lat)
-  console.log(lon)
+  var queryUrl2 = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
   fetch(queryUrl2)
     .then(function (response) {
       return response.json();
     })
-    .then(function (data) {
-      console.log(data)
-      lon = data.lon; // Uncaught (in promise) TypeError: Cannot read properties of undefined...
-      lat = data.lat;
+    .then(function (data) { // I think this needs different data... 
+      lon = data.lon; //  <-- not lat
+      lat = data.lat; // and lon?
     })
 };
 
+getCityData();
 
-// console.log(JSON.stringify(getLatLong)); // logs undefined
+// tested & functional
+searchButton.click(function (event) {
+  event.preventDefault();
+  cityName = inputText.val;
+  getCityData(cityName);
+});
+
+// tested & functional
+$("#input-city").on("keypress", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    cityName = inputText.val;
+    getCityData(cityName);
+  }
+});
